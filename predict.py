26b2cad35fd2bb2ff91f7bf39f20fe54306abf86
Abhay Kumar <abhaykumar714371@gmail.com>
2024-02-04 01:04:44 +0530

    wheat model

--- predict.py
+++ predict.py
@@ -12,10 +12,8 @@ def MakePrediction(image_path, model, classes):
     img_arr = tf.keras.applications.densenet.preprocess_input(img_arr)
     img_arr = np.array([img_arr])
     
-    # Make predictions using the model
     pred = model.predict(img_arr)
-    
-    # Get the predicted category
     category = classes[np.argmax(pred)]
-    
+    # print(category)
     return category
+
